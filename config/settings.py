d61ad66049bb44f3edd603b1a670ebe13125b972
Michael Provost <mike@provosthomedesign.com>
2026-05-29 12:30:42 -0400

    Fix: Update MEDIA_URL to use the production S3 bucket for media files

--- config/settings.py
+++ config/settings.py
@@ -362,7 +362,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
 
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+MEDIA_URL = "https://phd-media-prod.s3.us-east-1.amazonaws.com/media/"
 
 STORAGES = {
     "default": {
